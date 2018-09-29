f932fb7ad1dc726a7852063358cdb3931f3da7f5
songyanlin <songyanlin@tuandai.com>
2018-09-29 23:55:55 +0800

    add 01-flask-basic

---
+++ 01-flusk-basic/flusk1.py
@@ -0,0 +1,43 @@
+# from flask import Flask
+#
+# app = Flask(__name__)
+#
+#
+# @app.route('/')
+# def hello_world():
+#     return 'Hello World!'
+#
+#
+# if __name__ == '__main__':
+#     app.run()
+
+
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return 'Index Page'
+
+@app.route('/hello')
+def hello():
+    return 'Hello world'
+
+@app.route('/user/<username>')
+def show_user_profile(username):
+    return 'User %s' % username
+
+@app.route('/post/<int:post_id>')
+def show_post(post_id):
+    return 'Post %d' % post_id
+
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=9000, debug=True)
